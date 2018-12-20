23afe290d539a322e43034127691236e048693be
Matvey <volko-matvey@yandex.ru>
2018-12-20 16:35:12 +0500

    html+css

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Test</title>
+  <link rel="stylesheet" href="style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+  	<header>
+    	<div class="container">
+      		<div class="logo">Тест Promedia</div>
+    	</div>
+  	</header>
+  	<div class="container">
+    	<div class="wraper">
+			<form method="POST" action="#">
+				<textarea id="text" placeholder="Введите текст"></textarea>
+			</form>
+  			<div id="otvet"></div>
+		</div>
+		<button class="send" id="send" value="Submit">Отправить</button> 
+	</div>
+</body>
+</html>
